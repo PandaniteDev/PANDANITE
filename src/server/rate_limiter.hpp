b2213f0e4aa2977edcccd4773c8e2f0087e6bcee
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-02 16:08:25 +0100

    Update rate_limiter.hpp

--- src/server/rate_limiter.hpp
+++ src/server/rate_limiter.hpp
@@ -6,7 +6,7 @@ class RateLimiter {
     public:
         RateLimiter(size_t N, size_t seconds);
         bool limit(const std::string &s);
-    private:
+     private:
         const size_t N;
         const size_t seconds;
         std::map<std::string, std::chrono::steady_clock::time_point> c;
