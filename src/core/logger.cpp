49ca2eb89ae463b60b7914a678b540d33a142c8d
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-02 15:51:02 +0100

    Update logger.cpp

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -1,7 +1,7 @@
 #include "logger.hpp"
 
 list<string> Logger::buffer = list<string>();
-ofstream Logger::file = ofstream();
+ofstream Logger::file = ofstream( );
 std::mutex Logger::console_lock;
 std::mutex Logger::file_mutex;
 std::mutex Logger::buffer_mutex;
