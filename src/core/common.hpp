e4f092d9754ad3cf1ee91fb85095aef28ea20f6c
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-02 15:53:19 +0100

    Update common.hpp

--- src/core/common.hpp
+++ src/core/common.hpp
@@ -23,7 +23,7 @@ typedef std::array<uint8_t,64> TransactionSignature;
 
 typedef map<PublicWalletAddress,TransactionAmount> LedgerState;
 typedef std::array<uint8_t, 32> SHA256Hash;
-typedef std::array<uint8_t, 20> RIPEMD160Hash;
+typedef std::array<uint8_t, 20>  RIPEMD160Hash;
 
 #define NULL_SHA256_HASH SHA256Hash({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
 #define NULL_KEY SHA256Hash({0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0})
