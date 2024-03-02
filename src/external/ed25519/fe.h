839b6d486ac8b2880a4d8bbffe02406914cfbc20
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-02 16:01:51 +0100

    Update fe.h

--- src/external/ed25519/fe.h
+++ src/external/ed25519/fe.h
@@ -20,7 +20,7 @@ void fe_0(fe h);
 void fe_1(fe h);
 
 void fe_frombytes(fe h, const unsigned char *s);
-void fe_tobytes(unsigned char *s, const fe h);
+void fe_tobytes(unsigned char *s, const fe  h);
 
 void fe_copy(fe h, const fe f);
 int fe_isnegative(const fe f);
