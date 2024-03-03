8b4893529759001454b1bdf23c3ef032a26d53e1
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-03 12:12:33 +0100

    Update data_store.cpp
    make the code more robust, readable, and in line with modern C++ best practices.

--- src/server/data_store.cpp
+++ src/server/data_store.cpp
@@ -1,54 +1,42 @@
 #include "data_store.hpp"
 #include <thread>
-
-#ifdef _WIN32
+#include <memory>
 #include <filesystem>
-#else
-#include <experimental/filesystem>
-#endif
 
-DataStore::DataStore() {
-    this->db = NULL;
+DataStore::DataStore() : db(nullptr) {}
+
+void DataStore::closeDB() {
+    db.reset();  // Use smart pointer to automatically manage memory.
 }
 
- void DataStore::closeDB() {
-    delete db;
-}
-
-string DataStore::getPath() const{
-    return this->path;
+std::string DataStore::getPath() const {
+    return path;
 }
 
 void DataStore::clear() {
-    leveldb::Iterator* it = db->NewIterator(leveldb::ReadOptions());
+    std::unique_ptr<leveldb::Iterator> it(db->NewIterator(leveldb::ReadOptions()));
     for (it->SeekToFirst(); it->Valid(); it->Next()) {
-        string key = it->key().ToString();
+        std::string key = it->key().ToString();
         leveldb::Status status = db->Delete(leveldb::WriteOptions(), key);
-        if(!status.ok()) throw std::runtime_error("Could not clear data store : " + status.ToString());
+        if (!status.ok()) throw std::runtime_error("Could not clear data store : " + status.ToString());
     }
-    assert(it->status().ok()); 
-    delete it;
+    assert(it->status().ok());
 }
 
 void DataStore::deleteDB() {
     leveldb::Options options;
-    leveldb::Status status = leveldb::DestroyDB(this->path, options);
-#ifdef _WIN32
-    filesystem::remove_all(this->path); 
-#else
-    experimental::filesystem::remove_all(this->path); 
-#endif
-    if(!status.ok()) throw std::runtime_error("Could not close DataStore db : " + status.ToString());
+    leveldb::Status status = leveldb::DestroyDB(path, options);
+    std::filesystem::remove_all(path);  // Use std::filesystem consistently.
+    if (!status.ok()) throw std::runtime_error("Could not close DataStore db : " + status.ToString());
 }
 
-void DataStore::init(string path) {
-    if (this->db) {
-        this->closeDB();
-    }
-    this->path = path;
+void DataStore::init(std::string path) {
+    closeDB();  // Ensure previous resources are released.
+    this->path = std::move(path);  // Use move semantics for efficiency.
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+
     leveldb::Options options;
     options.create_if_missing = true;
-    leveldb::Status status = leveldb::DB::Open(options, path, &this->db);
-    if(!status.ok()) throw std::runtime_error("Could not write DataStore db : " + status.ToString());
+    leveldb::Status status = leveldb::DB::Open(options, this->path, &db);
+    if (!status.ok()) throw std::runtime_error("Could not write DataStore db : " + status.ToString());
 }
