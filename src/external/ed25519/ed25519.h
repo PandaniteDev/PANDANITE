58922cd8f8619e3dd6b4d0d39a6ceaddd912a9a1
PandaniteDev <161952866+PandaniteDev@users.noreply.github.com>
2024-03-03 18:45:11 +0100

    Update ed25519.h
    But fixed regarding RPC

--- src/external/ed25519/ed25519.h
+++ src/external/ed25519/ed25519.h
@@ -1,5 +1,5 @@
-#ifndef ED25519_H
-#define ED25519_H
+#ifndef ED25519_H_
+#define ED25519_H_
 
 #include <stddef.h>
 
@@ -15,23 +15,32 @@
     #define ED25519_DECLSPEC
 #endif
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 #ifndef ED25519_NO_SEED
-int ED25519_DECLSPEC ed25519_create_seed(unsigned char *seed);
+    // Generates an Ed25519 seed.
+    int ED25519_DECLSPEC ed25519_create_seed(unsigned char *seed);
 #endif
 
-void ED25519_DECLSPEC ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed);
-void ED25519_DECLSPEC ed25519_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key);
-int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
-void ED25519_DECLSPEC ed25519_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar);
-void ED25519_DECLSPEC ed25519_key_exchange(unsigned char *shared_secret, const unsigned char *public_key, const unsigned char *private_key);
+    // Generates an Ed25519 key pair from a seed.
+    void ED25519_DECLSPEC ed25519_create_keypair(unsigned char *public_key, unsigned char *private_key, const unsigned char *seed);
 
- 
+    // Signs a message using the Ed25519 key pair.
+    void ED25519_DECLSPEC ed25519_sign(unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key, const unsigned char *private_key);
+
+    // Verifies an Ed25519 signature.
+    int ED25519_DECLSPEC ed25519_verify(const unsigned char *signature, const unsigned char *message, size_t message_len, const unsigned char *public_key);
+
+    // Adds a scalar to the Ed25519 key pair.
+    void ED25519_DECLSPEC ed25519_add_scalar(unsigned char *public_key, unsigned char *private_key, const unsigned char *scalar);
+
+    // Performs an Ed25519 key exchange.
+    void ED25519_DECLSPEC ed25519_key_exchange(unsigned char *shared_secret, const unsigned char *public_key, const unsigned char *private_key);
+
+#ifdef __cplusplus
 }
 #endif
 
-#endif
+#endif // ED25519_H_
